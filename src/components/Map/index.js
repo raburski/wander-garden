d06d860d9ee0fec939385bfca963c75224ceea7d
Marcin Raburski <rabursky@gmail.com>
2023-04-22 15:14:13 +0800

    feat: bouncing hotel pins

---
+++ src/components/Map/index.js
@@ -0,0 +1,96 @@
+import { Wrapper as GoogleMapsWrapper } from "@googlemaps/react-wrapper"
+import { useEffect, useRef } from "react"
+import useMapsStyles from "./useMapStyles";
+
+function getBoundsZoomLevel(bounds, mapDim) {
+    var WORLD_DIM = { height: 256, width: 256 };
+    var ZOOM_MAX = 21;
+
+    function latRad(lat) {
+        var sin = Math.sin(lat * Math.PI / 180);
+        var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+    }
+
+    function zoom(mapPx, worldPx, fraction) {
+        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+    }
+
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+
+    var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
+    
+    var lngDiff = ne.lng() - sw.lng();
+    var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+    
+    var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
+    var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
+
+    return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
+function MyMapComponent({ markers, bouncingMarkerIndex }) {
+    const ref = useRef()
+    const mapRef = useRef()
+    const currentMarkerIndex = useRef(undefined)
+    const markersRef = useRef()
+    const styles = useMapsStyles()
+  
+    useEffect(() => {
+        const google = window.google
+
+        // Calculate initial zoom and center
+        const MIN_ZOOM = 13
+        const MAP_PADDING = 32
+        const mapDimensions = { width: ref.current.offsetWidth - MAP_PADDING * 2, height: ref.current.offsetHeight - MAP_PADDING * 2 }
+        const bounds = new google.maps.LatLngBounds()
+        markers.forEach(marker => bounds.extend(marker.position))
+        const center = bounds.getCenter()
+        const zoom = Math.min(MIN_ZOOM, getBoundsZoomLevel(bounds, mapDimensions))
+
+        const map = new google.maps.Map(ref.current, {
+            center,
+            zoom,
+            controlSize: 26,
+            backgroundColor: 'none',
+            styles,
+        })
+
+        // create markers
+        const _markers = markers.map(marker => {
+            return new google.maps.Marker(marker)
+        })
+
+        _markers.forEach(marker => marker.setMap(map))
+        markersRef.current = _markers
+        mapRef.current = map
+    }, [])
+
+    useEffect(() => {
+        mapRef.current.setOptions({ styles })
+    }, [styles])
+
+    useEffect(() => {
+        const markers = markersRef.current
+        if (!markers || markers.length < bouncingMarkerIndex) return
+        if (bouncingMarkerIndex === undefined) return
+        if (currentMarkerIndex.current !== undefined) {
+            markers[currentMarkerIndex.current].setAnimation(null)
+        }
+
+        markers[bouncingMarkerIndex].setAnimation(window.google.maps.Animation.BOUNCE)
+        currentMarkerIndex.current = bouncingMarkerIndex
+
+    }, [bouncingMarkerIndex])
+  
+    return <div ref={ref} id="map" style={{display: 'flex', flex: 1, alignSelf: 'stretch', backgroundColor: 'transparent'}}/>;
+}
+
+export default function GoogleMaps(props) {
+    return (
+        <GoogleMapsWrapper apiKey="">
+            <MyMapComponent {...props}/>
+        </GoogleMapsWrapper>
+    )
+}
--- src/routes/Trip/index.js
+++ src/routes/Trip/index.js
@@ -1,5 +1,4 @@
 import { useTimelineGroup } from 'domain/timeline'
-import { Wrapper as GoogleMapsWrapper } from "@googlemaps/react-wrapper";
 import { titleFromLocationHighlights } from 'domain/timeline/groups'
 import { useParams } from 'react-router'
 import Page from '../../components/Page'
@@ -12,10 +11,10 @@ import GroupBar from 'routes/Timeline/GroupBar'
 import { getDaysAndRangeText } from 'date'
 import useGroups, { GroupType } from './useGroups'
 import Phase from './Phase'
-import { useEffect, useRef } from 'react';
+import { useState } from 'react';
 import Separator from 'components/Separator';
 import { Column, Row } from 'components/container';
-import useMapsStyles from './useMapStyles';
+import Map from 'components/Map'
 
 const EventsContainer = styled('div')`
     display: flex;
@@ -52,7 +51,7 @@ function getGroupTitle(group) {
     }
 }
 
-function Group({ group }) {
+function Group({ group, onPhaseHighlight }) {
     const [days] = getDaysAndRangeText(group.since, group.until)
     // TODO: support unknown type
     // if (group.type === GroupType.Unknown) {
@@ -61,101 +60,28 @@ function Group({ group }) {
     return (
         <GroupContainer>
             <GroupHeader countryCodes={[group?.location?.cc]} title={getGroupTitle(group)} days={days}/>
-            {group.phases.map(phase => <Phase phase={phase} />)}
+            {group.phases.map(phase => <Phase phase={phase} onMouseEnter={() => onPhaseHighlight(phase)}/>)}
         </GroupContainer>
     )
 }
 
 
-function GroupsPanel({ groups, ...props }) {
+function GroupsPanel({ groups, onPhaseHighlight, ...props }) {
     return (
         <Panel flex {...props}>
-            {groups.map(group => <Group group={group}/>)}
+            {groups.map(group => <Group group={group} onPhaseHighlight={onPhaseHighlight}/>)}
         </Panel>
     )
 }
 
-function getBoundsZoomLevel(bounds, mapDim) {
-    var WORLD_DIM = { height: 256, width: 256 };
-    var ZOOM_MAX = 21;
-
-    function latRad(lat) {
-        var sin = Math.sin(lat * Math.PI / 180);
-        var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-    }
-
-    function zoom(mapPx, worldPx, fraction) {
-        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-    }
-
-    var ne = bounds.getNorthEast();
-    var sw = bounds.getSouthWest();
-
-    var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
-    
-    var lngDiff = ne.lng() - sw.lng();
-    var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
-    
-    var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
-    var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
-
-    return Math.min(latZoom, lngZoom, ZOOM_MAX);
-}
-
-function MyMapComponent({ stays }) {
-    const ref = useRef()
-    const mapRef = useRef()
-    const mapStyles = useMapsStyles()
-  
-    useEffect(() => {
-        const google = window.google
-
-        // Calculate initial zoom and center
-        const MIN_ZOOM = 13
-        const MAP_PADDING = 20
-        const mapDimensions = { width: ref.current.offsetWidth - MAP_PADDING * 2, height: ref.current.offsetHeight - MAP_PADDING * 2 }
-        const bounds = new google.maps.LatLngBounds()
-        // TODO: Add checkin locations from the trip too
-        stays.forEach(stay => bounds.extend({ lat: stay.location.lat, lng: stay.location.lng}))
-        const center = bounds.getCenter()
-        const zoom = Math.min(MIN_ZOOM, getBoundsZoomLevel(bounds, mapDimensions))
-
-        const map = new google.maps.Map(ref.current, {
-            center,
-            zoom,
-            controlSize: 26,
-            backgroundColor: 'none',
-            styles: mapStyles,
-        })
-
-        // create markers
-        const stayMarkers = stays.map(stay => {
-            return new google.maps.Marker({
-                position: new google.maps.LatLng(stay.location.lat, stay.location.lng)
-            })
-        })
-
-        stayMarkers.forEach(marker => marker.setMap(map))
-
-        mapRef.current = map
-    })
-
-    useEffect(() => {
-        mapRef.current.setOptions({ styles: mapStyles })
-    }, [mapStyles])
-  
-    return <div ref={ref} id="map" style={{display: 'flex', flex: 1, alignSelf: 'stretch', backgroundColor: 'transparent'}}/>;
-}
-
-function Map({ trip, style = {} }) {
+function TripMap({ trip, style = {}, highlightedPhase }) {
     const stays = trip ? trip.phases.map(phase => phase.type === PhaseType.Stay ? phase.stay : undefined).filter(Boolean) : []
+    const markers = stays.map(stay => ({ position: stay.location }))
+    const highlightedStayIndex = highlightedPhase ? stays.findIndex(stay => highlightedPhase.stay === stay) : undefined
 
     return (
         <Panel style={{flex: 1, alignSelf: 'stretch', flexShrink: 2, ...style }} contentStyle={{ flex: 1, display: 'flexbox', alignSelf: 'stretch'}}>
-            <GoogleMapsWrapper apiKey="AIzaSyCbFg09TiWP_fS1612Ir_bP3hFqW1mizA4">
-            {stays.length > 0 ? <MyMapComponent stays={stays}/> : null}
-            </GoogleMapsWrapper>
+            <Map markers={markers} bouncingMarkerIndex={highlightedStayIndex}/>
         </Panel>
     )
 }
@@ -163,6 +89,7 @@ function Map({ trip, style = {} }) {
 
 export default function Trip() {
     const { id } = useParams()
+    const [highlightedPhase, setHighlightedPhase] = useState()
     const group = useTimelineGroup(id)
 
     const trip = useTrip(group?.since, group?.until)
@@ -170,12 +97,8 @@ export default function Trip() {
 
     if (!group) { return null }
 
-    // console.log('groups', groups)
-
     const header = `Trip to ${titleFromLocationHighlights(group.highlights)}`
     const leftToRightPhases = [...group.phases].reverse()
-
-    // console.log('trip', trip)
 
     return (
         <Page header={header} showBackButton>
@@ -185,10 +108,10 @@ export default function Trip() {
                     <Panel style={{paddingTop: 18}}>
                         <EventsContainer>{leftToRightPhases.map(event => <GroupEvent key={event.id} event={event}/>)}</EventsContainer>
                     </Panel>
-                    <GroupsPanel header={`${moment(group?.since).format('DD/MM/YYYY')} - ${moment(group?.until).format('DD/MM/YYYY')}`} groups={groups} />
+                    <GroupsPanel header={`${moment(group?.since).format('DD/MM/YYYY')} - ${moment(group?.until).format('DD/MM/YYYY')}`} groups={groups} onPhaseHighlight={setHighlightedPhase}/>
                 </Column>
                 <Separator />
-                <Map style={{paddingTop: 18}} trip={trip}/>
+                <TripMap style={{paddingTop: 18}} trip={trip} highlightedPhase={highlightedPhase}/>
             </Row>
         </Page>
     )
